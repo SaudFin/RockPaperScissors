c1df8bf2ee63d3f8e41f5e96cad64d1e143d883e
SaudFin <developer.saud@gmail.com>
2020-08-08 11:37:59 +0300

    game function is added to determine the winner of the game

--- main.js
+++ main.js
@@ -1,5 +1,7 @@
 let arrayPlay = ["ROCK", "PAPER", "SCISSORS"];
-
+// to keep score for the player and computer for the game function
+let playerScore = 0;
+let computerScore = 0;
 const computerPlay = () => {
   let random = Math.floor(Math.random() * Math.floor(3));
   return arrayPlay[random];
@@ -12,8 +14,10 @@ const playRound = (playerSelection, computerSelection) => {
   //  when playerSelection is Rock
   if (playerSelection === "ROCK") {
     if (computerSelection === "PAPER") {
+      computerScore++;
       return "You Lose! Paper beats Rock";
     } else if (computerSelection === "SCISSORS") {
+      playerScore++;
       return "You Win, Rock beats Scissors";
     } else {
       return "Tie";
@@ -22,9 +26,11 @@ const playRound = (playerSelection, computerSelection) => {
   //  when playerSelction is Paper
   else if (playerSelection === "PAPER") {
     if (computerSelection === "Rock") {
+      playerScore++;
       return "You Win, Paper beats Rock";
     } else if (computerSelection === "SCISSORS") {
-      return "You Lost! Scissors beat Paper";
+      computerScore++;
+      return "You Lose! Scissors beat Paper";
     } else {
       return "Tie";
     }
@@ -32,15 +38,31 @@ const playRound = (playerSelection, computerSelection) => {
   //   when PlayerSelection is Scissors
   else {
     if (computerSelection === "Rock") {
+      computerScore++;
       return "You Lose! Rock beats Scissors";
     } else if (computerSelection === "Paper") {
+      playerScore++;
       return "You Win, Scissors beat Paper";
     } else {
       return "Tie";
     }
   }
 };
-
 const playerSelection = "rock";
-const computerSelection = computerPlay();
-console.log(playRound(playerSelection, computerSelection));
+// const computerSelection = computerPlay();
+const game = () => {
+  for (let i = 0; i < 5; i++) {
+    // console.log(computerPlay());
+    console.log(playRound(playerSelection, computerPlay()));
+    console.log(` ${playerScore} :${computerScore}`);
+  }
+  if (playerScore > computerScore) {
+    return "you have won the game";
+  } else if (computerScore > playerScore) {
+    return "you have lost the game";
+  } else {
+    return "it is a tie bro";
+  }
+};
+
+console.log(game());
