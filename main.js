70ace5e8a9d83d88a934c88058bf4d2304bb9313
SaudFin <developer.saud@gmail.com>
2020-08-23 21:09:40 +0300

    user can play by clicking on the button, JS is linked

--- main.js
+++ main.js
@@ -1,4 +1,8 @@
 let arrayPlay = ["ROCK", "PAPER", "SCISSORS"];
+let rock = document.getElementById("rock");
+let paper = document.getElementById("paper");
+let scissors = document.getElementById("scissors");
+
 // to keep score for the player and computer for the game function
 let playerScore = 0;
 let computerScore = 0;
@@ -49,15 +53,27 @@ const playRound = (playerSelection, computerSelection) => {
     }
   }
 };
-
-const game = () => {
-  for (let i = 0; i < 5; i++) {
-    // below line is changed because we will be taking the player selection from his click on the button
-    // let playerSelection = window.prompt();
-
-    console.log(playRound(playerSelection, computerPlay()));
-    console.log(` ${playerScore} :${computerScore}`);
-  }
+// to get the player selection from the user clicks on the buttons
+// const playerSelection = () => {
+//   if (rock.onclick) {
+//     return "ROCK";
+//   }
+//   if (paper.onclick) {
+//     return "PAPER";
+//   }
+//   if (scissors.onclick) {
+//     return "scissors";
+//   }
+// };
+const game = playerSelection => {
+  // for (let i = 0; i < 5; i++) {
+  // below line is changed because we will be taking the player selection from his click on the button
+  // let playerSelection = window.prompt();
+  // let playerSelection = console.log(
+  playRound(playerSelection, computerPlay());
+  // );
+  console.log(` ${playerScore} :${computerScore}`);
+  // }
   if (playerScore > computerScore) {
     return "you have won the game";
   } else if (computerScore > playerScore) {
@@ -67,4 +83,14 @@ const game = () => {
   }
 };
 
-console.log(game());
+rock.addEventListener("click", function() {
+  game("ROCK");
+});
+
+paper.addEventListener("click", function() {
+  game("PAPER");
+});
+
+scissors.addEventListener("click", function() {
+  game("SCISSORS");
+});
