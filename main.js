210522a69f49322d0f2a5fc5e73904b51b2ff03e
SaudFin <developer.saud@gmail.com>
2020-08-24 05:37:40 +0300

    full game is functional with display of the secore, and a alert for the winner after the fifth round

--- main.js
+++ main.js
@@ -2,7 +2,10 @@ let arrayPlay = ["ROCK", "PAPER", "SCISSORS"];
 let rock = document.getElementById("rock");
 let paper = document.getElementById("paper");
 let scissors = document.getElementById("scissors");
-
+let computerScoreDisplay = document.getElementById("computerScore");
+let playerScoreDisplay = document.getElementById("playerScore");
+// to keep track of the rounds
+let round = 0;
 // to keep score for the player and computer for the game function
 let playerScore = 0;
 let computerScore = 0;
@@ -13,73 +16,68 @@ const computerPlay = () => {
 
 // function to figure who won the round
 const playRound = (playerSelection, computerSelection) => {
-  // the below line is commented because we will be putting value in the elements
-  // playerSelection = playerSelection.toUpperCase();
-
   //  when playerSelection is Rock
   if (playerSelection === "ROCK") {
     if (computerSelection === "PAPER") {
       computerScore++;
-      return "You Lose! Paper beats Rock";
     } else if (computerSelection === "SCISSORS") {
       playerScore++;
-      return "You Win, Rock beats Scissors";
     } else {
-      return "Tie";
+      console.log("Tie");
     }
   }
   //  when playerSelction is Paper
   else if (playerSelection === "PAPER") {
     if (computerSelection === "ROCK") {
       playerScore++;
-      return "You Win, Paper beats Rock";
     } else if (computerSelection === "SCISSORS") {
       computerScore++;
-      return "You Lose! Scissors beat Paper";
     } else {
-      return "Tie";
+      console.log("Tie");
     }
   }
   //   when PlayerSelection is Scissors
   else {
     if (computerSelection === "ROCK") {
       computerScore++;
-      return "You Lose! Rock beats Scissors";
     } else if (computerSelection === "PAPER") {
       playerScore++;
-      return "You Win, Scissors beat Paper";
     } else {
-      return "Tie";
+      console.log("Tie");
     }
   }
+  computerScoreDisplay.innerHTML = computerScore;
+  playerScoreDisplay.innerHTML = playerScore;
 };
-// to get the player selection from the user clicks on the buttons
-// const playerSelection = () => {
-//   if (rock.onclick) {
-//     return "ROCK";
-//   }
-//   if (paper.onclick) {
-//     return "PAPER";
-//   }
-//   if (scissors.onclick) {
-//     return "scissors";
-//   }
-// };
+// to check who won the five rounds
+const gameWinner = (playerScore, computerScore) => {
+  computerScoreDisplay.innerHTML = computerScore;
+  playerScoreDisplay.innerHTML = playerScore;
+  setTimeout(function() {
+    if (playerScore > computerScore) {
+      alert("you have won the game");
+    } else if (computerScore > playerScore) {
+      alert("you have lost the game");
+    } else {
+      alert("it is a tie bro");
+    }
+  }, 200);
+};
 const game = playerSelection => {
-  // for (let i = 0; i < 5; i++) {
-  // below line is changed because we will be taking the player selection from his click on the button
-  // let playerSelection = window.prompt();
-  // let playerSelection = console.log(
+  round++;
+  console.log(round);
   playRound(playerSelection, computerPlay());
-  // );
   console.log(` ${playerScore} :${computerScore}`);
-  // }
-  if (playerScore > computerScore) {
-    return "you have won the game";
-  } else if (computerScore > playerScore) {
-    return "you have lost the game";
-  } else {
-    return "it is a tie bro";
+
+  if (round === 5) {
+    round = 0;
+    gameWinner(playerScore, computerScore);
+    setTimeout(function() {
+      playerScore = 0;
+      computerScore = 0;
+      computerScoreDisplay.innerHTML = computerScore;
+      playerScoreDisplay.innerHTML = playerScore;
+    }, 500);
   }
 };
 
